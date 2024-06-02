51b64070e01c53c6ad1c16a5c587db55f20822bb
dearaujofelipe <mgdefelipe@outlook.com>
2024-06-02 10:53:08 -0300

    'imagepicker'

---
+++ components/Places/ImagePicker.js
@@ -0,0 +1,88 @@
+import { Alert, Button, Image, StyleSheet, Text, View } from 'react-native';
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from 'expo-image-picker';
+import { useState } from 'react';
+
+import { Colors } from '../../constants/colors';
+
+function ImagePicker() {
+  const [pickedImage, setPickedImage] = useState();
+
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  async function verifyPermissions() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        'Insufficient Permissions!',
+        'You need to grant camera permissions to use this app.'
+      );
+      return false;
+    }
+
+    return true;
+  }
+
+  async function takeImageHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    const result = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+
+    console.log('Image response: ', result);
+
+    if (!result.canceled && result.assets && result.assets.length > 0) {
+      const uri = result.assets[0].uri;
+      console.log('Image URI: ', uri);
+      setPickedImage(uri);
+    } else {
+      console.log('Image picking was cancelled.');
+    }
+  }
+
+  return (
+    <View>
+      <View style={styles.imagePreview}>
+        {pickedImage ? (
+          <Image style={styles.image} source={{ uri: pickedImage }} />
+        ) : (
+          <Text>No image taken yet.</Text>
+        )}
+      </View>
+      <Button title="Take Image" onPress={takeImageHandler} />
+    </View>
+  );
+}
+
+export default ImagePicker;
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: '100%',
+    height: 200,
+    marginVertical: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
